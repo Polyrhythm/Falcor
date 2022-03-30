9de17ae7f41839ed6fa75ca2b4a561d5ddab6e36
RyanJJones <jones@kojimaproductions.jp>
2022-03-30 09:51:28 +0900

    add tutorial renderpass + spectral renderpass

---
+++ Source/RenderPasses/SpectralPathTracer/SpectralPathTracer.cpp
@@ -0,0 +1,292 @@
+/***************************************************************************
+ # Copyright (c) 2015-21, NVIDIA CORPORATION. All rights reserved.
+ #
+ # Redistribution and use in source and binary forms, with or without
+ # modification, are permitted provided that the following conditions
+ # are met:
+ #  * Redistributions of source code must retain the above copyright
+ #    notice, this list of conditions and the following disclaimer.
+ #  * Redistributions in binary form must reproduce the above copyright
+ #    notice, this list of conditions and the following disclaimer in the
+ #    documentation and/or other materials provided with the distribution.
+ #  * Neither the name of NVIDIA CORPORATION nor the names of its
+ #    contributors may be used to endorse or promote products derived
+ #    from this software without specific prior written permission.
+ #
+ # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS "AS IS" AND ANY
+ # EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ # PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ # CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ # EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ # PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ # PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ # OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **************************************************************************/
+#include "SpectralPathTracer.h"
+#include "RenderGraph/RenderPassHelpers.h"
+
+const RenderPass::Info SpectralPathTracer::kInfo { "SpectralPathTracer", "Fork of minimal pathtracer to include spectral rendering." };
+
+// Don't remove this. it's required for hot-reload to function properly
+extern "C" FALCOR_API_EXPORT const char* getProjDir()
+{
+    return PROJECT_DIR;
+}
+
+extern "C" FALCOR_API_EXPORT void getPasses(Falcor::RenderPassLibrary& lib)
+{
+    lib.registerPass(SpectralPathTracer::kInfo, SpectralPathTracer::create);
+}
+
+namespace
+{
+    const char kShaderFile[] = "RenderPasses/SpectralPathTracer/SpectralPathTracer.rt.slang";
+
+    // raytracing settings
+    // the small the better
+    const uint32_t kMaxPayloadSizeBytes = 72u;
+    const uint32_t kMaxRecursionDepth = 2u;
+
+    const char kInputViewDir[] = "viewW";
+
+    const ChannelList kInputChannels =
+    {
+        {"vbuffer", "gVBuffer", "Visibility buffer in packed format"},
+        { kInputViewDir, "gViewW", "World-space view direction (xyz float format)", true /* optional */ },
+    };
+
+    const ChannelList kOutputChannels =
+    {
+        { "color", "gOutputColor", "Output color (sum of direct and indirect)", false, ResourceFormat::RGBA32Float },
+    };
+
+    const char kMaxBounces[] = "maxBounces";
+    const char kComputeDirect[] = "computeDirect";
+    const char kUseImportanceSampling[] = "useImportanceSampling";
+}
+
+SpectralPathTracer::SharedPtr SpectralPathTracer::create(RenderContext* pRenderContext, const Dictionary& dict)
+{
+    return SharedPtr(new SpectralPathTracer(dict));
+}
+
+SpectralPathTracer::SpectralPathTracer(const Dictionary& dict)
+    : RenderPass(kInfo)
+{
+    parseDictionary(dict);
+
+    // create a sample gen
+    mpSampleGenerator = SampleGenerator::create(SAMPLE_GENERATOR_UNIFORM);
+    FALCOR_ASSERT(mpSampleGenerator);
+}
+
+void SpectralPathTracer::parseDictionary(const Dictionary& dict)
+{
+    for (const auto& [key, value] : dict)
+    {
+        if (key == kMaxBounces) mMaxBounces = value;
+        else if (key == kComputeDirect) mComputeDirect = value;
+        else if (key == kUseImportanceSampling) mUseImportanceSampling = value;
+        else logWarning("Unknown field '{}' in Spectral PathTracer dictionary.", key);
+    }
+}
+
+Dictionary SpectralPathTracer::getScriptingDictionary()
+{
+    Dictionary d;
+    d[kMaxBounces] = mMaxBounces;
+    d[kComputeDirect] = mComputeDirect;
+    d[kUseImportanceSampling] = mUseImportanceSampling;
+    return d;
+}
+
+RenderPassReflection SpectralPathTracer::reflect(const CompileData& compileData)
+{
+    RenderPassReflection reflector;
+
+    addRenderPassInputs(reflector, kInputChannels);
+    addRenderPassOutputs(reflector, kOutputChannels);
+
+    return reflector;
+}
+
+void SpectralPathTracer::execute(RenderContext* pRenderContext, const RenderData& renderData)
+{
+    // Update refresh flag if options that affect the output have changed.
+    auto& dict = renderData.getDictionary();
+    if (mOptionsChanged)
+    {
+        auto flags = dict.getValue(kRenderPassRefreshFlags, RenderPassRefreshFlags::None);
+        dict[Falcor::kRenderPassRefreshFlags] = flags | Falcor::RenderPassRefreshFlags::RenderOptionsChanged;
+        mOptionsChanged = false;
+    }
+
+    if (!mpScene)
+    {
+        for (auto it : kOutputChannels)
+        {
+            Texture* pDst = renderData[it.name]->asTexture().get();
+            if (pDst)
+            {
+                pRenderContext->clearTexture(pDst);
+            }
+        }
+        return;
+    }
+
+    if (is_set(mpScene->getUpdates(), Scene::UpdateFlags::GeometryChanged))
+    {
+        throw RuntimeError("SpectralPathTracer: This render pass does not support scene geometry changes.");
+    }
+
+    // request the light collection if emissives lights are enabled
+    if (mpScene->getRenderSettings().useEmissiveLights)
+    {
+        mpScene->getLightCollection(pRenderContext);
+    }
+
+    // depth of field
+    const bool useDOF = mpScene->getCamera()->getApertureRadius() > 0.0f;
+    if (useDOF && renderData[kInputViewDir] == nullptr)
+    {
+        logWarning("Depth of field requires the '{}' input. Expect incorrect shading.", kInputViewDir);
+    }
+
+    // Specialize program
+    // These defines should not modify the program vars. Do not trigger program vars re-creation
+    mTracer.pProgram->addDefine("MAX_BOUNCES", std::to_string(mMaxBounces));
+    mTracer.pProgram->addDefine("COMPUTE_DIRECT", mComputeDirect ? "1" : "0");
+    mTracer.pProgram->addDefine("USE_IMPORTANCE_SAMPLING", mUseImportanceSampling ? "1" : "0");
+    mTracer.pProgram->addDefine("USE_ANALYTIC_LIGHTS", mpScene->useAnalyticLights() ? "1" : "0");
+    mTracer.pProgram->addDefine("USE_EMISSIVE_LIGHTS", mpScene->useEmissiveLights() ? "1" : "0");
+    mTracer.pProgram->addDefine("USE_ENV_LIGHT", mpScene->useEnvLight() ? "1" : "0");
+    mTracer.pProgram->addDefine("USE_ENV_BACKGROUND", mpScene->useEnvBackground() ? "1" : "0");
+
+    // for optional i/o resources, set 'is_valid_<name>' defines to inform the program of which ones it can access
+    mTracer.pProgram->addDefines(getValidResourceDefines(kInputChannels, renderData));
+    mTracer.pProgram->addDefines(getValidResourceDefines(kOutputChannels, renderData));
+
+    // prepare program vars, may trigger recompilation
+    if (!mTracer.pVars) prepareVars();
+    FALCOR_ASSERT(mTracer.pVars);
+
+    // set constants
+    auto var = mTracer.pVars->getRootVar();
+    var["CB"]["gFrameCount"] = mFrameCount;
+    var["CB"]["gPRNGDimension"] = dict.keyExists(kRenderPassPRNGDimension) ? dict[kRenderPassPRNGDimension] : 0u;
+
+    // bind i/o buffers, per-frame
+    auto bind = [&](const ChannelDesc& desc)
+    {
+        if (!desc.texname.empty())
+        {
+            var[desc.texname] = renderData[desc.name]->asTexture();
+        }
+    };
+    for (auto channel : kInputChannels) bind(channel);
+    for (auto channel : kOutputChannels) bind(channel);
+
+    // get dimensions of ray dispatch
+    const uint2 targetDim = renderData.getDefaultTextureDims();
+    FALCOR_ASSERT(targetDim.x > 0 && targetDim.y > 0);
+
+    // shoot the rays
+    mpScene->raytrace(pRenderContext, mTracer.pProgram.get(), mTracer.pVars, uint3(targetDim, 1));
+
+    mFrameCount++;
+}
+
+void SpectralPathTracer::renderUI(Gui::Widgets& widget)
+{
+    bool dirty = false;
+
+    dirty |= widget.var("Max bounces", mMaxBounces, 0u, 1u << 16);
+    widget.tooltip("Maximum path length for indirect illumination.\n0 = direct only\n1 = one indirect bounce, etc.", true);
+
+    dirty |= widget.checkbox("Evaluate direct illumination", mComputeDirect);
+    widget.tooltip("Compute direct illumination.\nIf disabled only indirect is computed (when max bounces > 0).", true);
+
+    dirty |= widget.checkbox("Use importance sampling", mUseImportanceSampling);
+    widget.tooltip("Use importance sampling for materials", true);
+
+    if (dirty)
+    {
+        mOptionsChanged = true;
+    }
+}
+
+void SpectralPathTracer::setScene(RenderContext* pRenderContext, const Scene::SharedPtr& pScene)
+{
+    // clear data from previous scene
+    // after changing, raytracing program should be recreated
+    mTracer.pProgram = nullptr;
+    mTracer.pBindingTable = nullptr;
+    mTracer.pVars = nullptr;
+    mFrameCount = 0;
+
+    // new scene
+    mpScene = pScene;
+
+    if (mpScene)
+    {
+        if (pScene->hasGeometryType(Scene::GeometryType::Custom))
+        {
+            logWarning("SpectralPathTracer: This render pass does not support custom primitives.");
+        }
+
+        RtProgram::Desc desc;
+        desc.addShaderLibrary(kShaderFile);
+        desc.setMaxPayloadSize(kMaxPayloadSizeBytes);
+        desc.setMaxAttributeSize(mpScene->getRaytracingMaxAttributeSize());
+        desc.setMaxTraceRecursionDepth(kMaxRecursionDepth);
+
+        mTracer.pBindingTable = RtBindingTable::create(2, 2, mpScene->getGeometryCount());
+        auto& sbt = mTracer.pBindingTable;
+        sbt->setRayGen(desc.addRayGen("rayGen"));
+        sbt->setMiss(0, desc.addMiss("scatterMiss"));
+        sbt->setMiss(1, desc.addMiss("shadowMiss"));
+
+        if (mpScene->hasGeometryType(Scene::GeometryType::TriangleMesh))
+        {
+            sbt->setHitGroup(0, mpScene->getGeometryIDs(Scene::GeometryType::TriangleMesh), desc.addHitGroup("scatterTriangleMeshClosestHit", "scatterTriangleMeshAnyHit"));
+            sbt->setHitGroup(1, mpScene->getGeometryIDs(Scene::GeometryType::TriangleMesh), desc.addHitGroup("", "shadowTriangleMeshAnyHit"));
+        }
+
+        if (mpScene->hasGeometryType(Scene::GeometryType::DisplacedTriangleMesh))
+        {
+            sbt->setHitGroup(0, mpScene->getGeometryIDs(Scene::GeometryType::DisplacedTriangleMesh), desc.addHitGroup("scatterTriangleMeshClosestHit", "displacedTriangleMeshIntersection"));
+            sbt->setHitGroup(1, mpScene->getGeometryIDs(Scene::GeometryType::DisplacedTriangleMesh), desc.addHitGroup("", "", "displacedTriangleMeshIntersection"));
+        }
+
+        if (mpScene->hasGeometryType(Scene::GeometryType::Curve))
+        {
+            sbt->setHitGroup(0, mpScene->getGeometryIDs(Scene::GeometryType::Curve), desc.addHitGroup("scatterCurveClosestHit", "curveIntersection"));
+            sbt->setHitGroup(1, mpScene->getGeometryIDs(Scene::GeometryType::Curve), desc.addHitGroup("", "", "curveIntersection"));
+        }
+
+        if (mpScene->hasGeometryType(Scene::GeometryType::SDFGrid))
+        {
+            sbt->setHitGroup(0, mpScene->getGeometryIDs(Scene::GeometryType::SDFGrid), desc.addHitGroup("scatterSdfGridClosestHit", "sdfGridIntersection"));
+            sbt->setHitGroup(1, mpScene->getGeometryIDs(Scene::GeometryType::SDFGrid), desc.addHitGroup("", "", "sdfGridIntersection"));
+        }
+
+        mTracer.pProgram = RtProgram::create(desc, mpScene->getSceneDefines());
+    }
+}
+
+void SpectralPathTracer::prepareVars()
+{
+    FALCOR_ASSERT(mpScene);
+    FALCOR_ASSERT(mTracer.pProgram);
+
+    mTracer.pProgram->addDefines(mpSampleGenerator->getDefines());
+    mTracer.pProgram->setTypeConformances(mpScene->getTypeConformances());
+
+    mTracer.pVars = RtProgramVars::create(mTracer.pProgram, mTracer.pBindingTable);
+
+    auto var = mTracer.pVars->getRootVar();
+    mpSampleGenerator->setShaderData(var);
+}
---
+++ Source/RenderPasses/SpectralPathTracer/SpectralPathTracer.h
@@ -0,0 +1,80 @@
+/***************************************************************************
+ # Copyright (c) 2015-21, NVIDIA CORPORATION. All rights reserved.
+ #
+ # Redistribution and use in source and binary forms, with or without
+ # modification, are permitted provided that the following conditions
+ # are met:
+ #  * Redistributions of source code must retain the above copyright
+ #    notice, this list of conditions and the following disclaimer.
+ #  * Redistributions in binary form must reproduce the above copyright
+ #    notice, this list of conditions and the following disclaimer in the
+ #    documentation and/or other materials provided with the distribution.
+ #  * Neither the name of NVIDIA CORPORATION nor the names of its
+ #    contributors may be used to endorse or promote products derived
+ #    from this software without specific prior written permission.
+ #
+ # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS "AS IS" AND ANY
+ # EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ # PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ # CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ # EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ # PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ # PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ # OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **************************************************************************/
+#pragma once
+#include "Falcor.h"
+#include "Utils/Sampling/SampleGenerator.h"
+
+using namespace Falcor;
+
+class SpectralPathTracer : public RenderPass
+{
+public:
+    using SharedPtr = std::shared_ptr<SpectralPathTracer>;
+
+    static const Info kInfo;
+
+    /** Create a new render pass object.
+        \param[in] pRenderContext The render context.
+        \param[in] dict Dictionary of serialized parameters.
+        \return A new object, or an exception is thrown if creation failed.
+    */
+    static SharedPtr create(RenderContext* pRenderContext = nullptr, const Dictionary& dict = {});
+
+    virtual Dictionary getScriptingDictionary() override;
+    virtual RenderPassReflection reflect(const CompileData& compileData) override;
+    virtual void execute(RenderContext* pRenderContext, const RenderData& renderData) override;
+    virtual void renderUI(Gui::Widgets& widget) override;
+    virtual void setScene(RenderContext* pRenderContext, const Scene::SharedPtr& pScene) override;
+    virtual bool onMouseEvent(const MouseEvent& mouseEvent) override { return false; }
+    virtual bool onKeyEvent(const KeyboardEvent& keyEvent) override { return false; }
+
+private:
+    SpectralPathTracer(const Dictionary& dict);
+    void parseDictionary(const Dictionary& dict);
+    void prepareVars();
+
+    // internal state
+    Scene::SharedPtr mpScene;
+    SampleGenerator::SharedPtr mpSampleGenerator;
+
+    // config
+    uint mMaxBounces = 3;
+    bool mComputeDirect = true; ///< Copute direct illumination (otherwise indirect only)
+    bool mUseImportanceSampling = true; ///< Use importance sampling for materials.
+
+    // runtime data
+    uint mFrameCount = 0;
+    bool mOptionsChanged = false;
+
+    struct
+    {
+        RtProgram::SharedPtr pProgram;
+        RtBindingTable::SharedPtr pBindingTable;
+        RtProgramVars::SharedPtr pVars;
+    } mTracer;
+};
